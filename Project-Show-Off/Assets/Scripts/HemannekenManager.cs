45990507b0c1f209b9cf5b7bd6da6761f55c27a5
lana <svetlasosnova@gmail.com>
2025-05-15 15:50:22 +0200

    feat: Spawn Points

---
+++ Project-Show-Off/Assets/Scripts/HemannekenManager.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class HemannekenManager : MonoBehaviour
+{
+    
+}
---
+++ Project-Show-Off/Assets/Scripts/SpawnPoints/SpawnPointsManager.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpawnPointsManager : MonoBehaviour
+{
+    public List<SpawnPoint> SpawnPoints { get; private set; }
+    public static event Action SpawnPointsInitialized;
+    
+    private void Awake()
+    {
+        SpawnPoints = FindObjectsByType<SpawnPoint>(FindObjectsInactive.Exclude, FindObjectsSortMode.None).ToList();
+        Debug.Log("Number of spawn points: " + SpawnPoints.Count);
+        SpawnPointsInitialized?.Invoke();
+    }
+}
