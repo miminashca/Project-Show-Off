bf02c544dffd95d437b9596fbe0b0896a55c0a7e
sabrecool <simeon15dorne@gmail.com>
2025-07-04 11:30:07 +0200

    WL kill sound is in

--- Project-Show-Off/Assets/Scripts/Audio/Enemies/WLSoundController.cs
+++ Project-Show-Off/Assets/Scripts/Audio/Enemies/WLSoundController.cs
@@ -1,74 +1,79 @@
 using UnityEngine;
 using FMODUnity;
-using FMOD.Studio; // Required for EventInstance and PLAYBACK_STATE
+using FMOD.Studio;
 
 public class WLSoundController : MonoBehaviour
 {
     [SerializeField] private EventReference WLKillNoise;
     [SerializeField] private EventReference WLLullaby;
 
-    // 1. To hold a reference to our playing lullaby sound
+    private EventInstance killNoiseInstance;
     private EventInstance lullabyInstance;
 
-    public void PlayKillNoise()
+    public void StartKillNoise()
     {
+        if (IsInstancePlaying(killNoiseInstance))
+        {
+            return;
+        }
+
         if (!WLKillNoise.IsNull)
         {
-            RuntimeManager.PlayOneShotAttached(WLKillNoise, gameObject);
+            killNoiseInstance = RuntimeManager.CreateInstance(WLKillNoise);
+            RuntimeManager.AttachInstanceToGameObject(killNoiseInstance, gameObject);
+            killNoiseInstance.start();
+        }
+    }
+
+    public void StopKillNoise()
+    {
+        if (killNoiseInstance.isValid())
+        {
+            // FIX 1: Specify the FMOD.Studio namespace to resolve ambiguity.
+            killNoiseInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            killNoiseInstance.release();
         }
     }
 
     public void PlayLullaby()
     {
-        // 3. Check if the lullaby is already playing before starting a new one
         if (IsInstancePlaying(lullabyInstance))
         {
-            // Optional: log that we are preventing a replay
-            // Debug.Log("Lullaby is already playing. Not starting a new one.");
             return;
         }
 
-        // If the event is not null and not already playing, create and play it
         if (!WLLullaby.IsNull)
         {
-            // Create an instance of the event
             lullabyInstance = RuntimeManager.CreateInstance(WLLullaby);
-
-            // Attach it to this GameObject for 3D spatialization
             RuntimeManager.AttachInstanceToGameObject(lullabyInstance, gameObject);
-
-            // Start the sound
             lullabyInstance.start();
-
-            // IMPORTANT: Release the instance. This tells FMOD it can free up this event
-            // from memory once it has finished playing. If you don't do this,
-            // you will have a memory leak.
             lullabyInstance.release();
         }
     }
 
-    // 2. A helper function to check the playback state of any EventInstance
     private bool IsInstancePlaying(EventInstance instance)
     {
-        // The instance must be valid to get its state
         if (!instance.isValid())
         {
             return false;
         }
 
-        // Get the current playback state
         instance.getPlaybackState(out PLAYBACK_STATE state);
-
-        // Return true if the state is not STOPPED
         return state != PLAYBACK_STATE.STOPPED;
     }
 
-    // Optional: It's good practice to stop any running instances when the object is destroyed
-    // to prevent orphaned sounds.
     private void OnDestroy()
     {
+        if (killNoiseInstance.isValid())
+        {
+            // FIX 2: Specify the FMOD.Studio namespace here as well.
+            killNoiseInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            killNoiseInstance.release();
+        }
+
         if (lullabyInstance.isValid())
         {
+            // FIX 3: And specify it here for the lullaby's cleanup.
             lullabyInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
             lullabyInstance.release();
         }
