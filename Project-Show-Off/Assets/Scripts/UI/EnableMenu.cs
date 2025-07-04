02a684a9c076b28d847725a4be6d12d946b22c99
BoggyWoggy <523487@student.saxion.nl>
2025-07-04 15:29:19 +0200

    Start Screen Band Aid Fix

---
+++ Project-Show-Off/Assets/Scripts/UI/EnableMenu.cs
@@ -0,0 +1,44 @@
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class EnableMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPanel; //enable this panel on start scene
+    [SerializeField] private Canvas loadingCanvas; //disable this canvas in environment scene
+
+    private void EnableMenuTab()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        menuPanel.SetActive(true);
+    }
+    private void DisableLoadingCanvas()
+    {
+        
+       loadingCanvas.enabled = false;
+        
+    }
+
+    private void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "StartScreen") //replace with your start scene name
+        {
+            EnableMenuTab();
+        }
+        else if (scene.name == "Environment Scene") //replace with your environment scene name
+        {
+            DisableLoadingCanvas();
+        }
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoad;
+    }
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoad;
+    }
+    
+
+}
